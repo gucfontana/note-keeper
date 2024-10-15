28dcf46409fea3453e91c8b57eca66ee5f8853fc
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-10-15 18:08:55 -0300

    implementado serviço e modelos de nota

---
+++ src/app/views/notas/services/nota.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
+import {
+  CadastroCategoria,
+  CategoriaCriada,
+  EdicaoCategoria,
+  CategoriaEditada,
+  CategoriaExcluida,
+  ListagemCategoria,
+  DetalhesCategoria,
+} from '../../categorias/models/categoria.models';
+import {
+  CadastroNota,
+  DetalhesNota,
+  EdicaoNota,
+  ListagemNota,
+  NotaCriada,
+  NotaEditada,
+  NotaExcluida,
+} from '../models/nota.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotaService {
+  private readonly url = `${environment.API_URL}/notas`;
+
+  constructor(private http: HttpClient) {}
+
+  cadastrar(novaNota: CadastroNota): Observable<NotaCriada> {
+    return this.http.post<NotaCriada>(this.url, novaNota);
+  }
+
+  editar(id: number, notaEditada: EdicaoNota): Observable<NotaEditada> {
+    const urlCompleto = `${this.url}/${id}`;
+
+    return this.http.put<NotaEditada>(urlCompleto, notaEditada);
+  }
+
+  excluir(id: number): Observable<NotaExcluida> {
+    const urlCompleto = `${this.url}/${id}`;
+
+    return this.http.delete<NotaExcluida>(urlCompleto);
+  }
+
+  selecionarTodos(): Observable<ListagemNota[]> {
+    return this.http.get<ListagemNota[]>(this.url);
+  }
+
+  selecionarPorId(id: number): Observable<DetalhesNota> {
+    const urlCompleto = `${this.url}/${id}`;
+
+    return this.http.get<DetalhesNota>(urlCompleto);
+  }
+}
