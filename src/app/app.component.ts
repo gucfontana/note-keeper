19c0222436d7cef158db9d4200da2f1805551052
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-10-15 16:36:47 -0300

    add angular material + navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { ShellComponent } from './core/components/shell/shell.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ShellComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'note-keeper';
-}
+export class AppComponent {}
