77e66ebc406d4d834704f5b3faf98c905ffcba27
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-10-15 18:56:21 -0300

    implementada edição de notas

--- src/app/views/notas/notas.routes.ts
+++ src/app/views/notas/notas.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListagemNotasComponent } from './listar/listagem-notas.component';
 import { CadastroNotaComponent } from './cadastrar/cadastro-nota.component';
+import { EdicaoNotaComponent } from './editar/edicao-nota.component';
 
 export const notasRoutes: Routes = [
   {
@@ -10,4 +11,5 @@ export const notasRoutes: Routes = [
   },
   { path: 'listar', component: ListagemNotasComponent },
   { path: 'cadastrar', component: CadastroNotaComponent },
+  { path: 'editar/:id', component: EdicaoNotaComponent },
 ];
