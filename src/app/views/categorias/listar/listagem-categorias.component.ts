c6e5b0349902a56d01f6f30213780f19f40bd1f5
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-10-15 17:11:42 -0300

    atualizando listagens

--- src/app/views/categorias/listar/listagem-categorias.component.ts
+++ src/app/views/categorias/listar/listagem-categorias.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ListagemCategoria } from '../models/categoria.models';
-import { NgForOf } from '@angular/common';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { CategoriaService } from '../services/categoria.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-listagem-categorias',
   standalone: true,
   imports: [
+    NgIf,
     NgForOf,
     RouterLink,
+    AsyncPipe,
     MatCardModule,
     MatButtonModule,
     MatIconModule,
@@ -23,15 +26,11 @@ import { CategoriaService } from '../services/categoria.service';
   styleUrl: './listagem-categorias.component.scss',
 })
 export class ListagemCategoriasComponent implements OnInit {
-  categorias: ListagemCategoria[];
+  categorias$?: Observable<ListagemCategoria[]>;
 
-  constructor(private categoriaService: CategoriaService) {
-    this.categorias = [];
-  }
+  constructor(private categoriaService: CategoriaService) {}
 
   ngOnInit(): void {
-    this.categoriaService.selecionarTodos().subscribe((res) => {
-      this.categorias = res;
-    });
+    this.categorias$ = this.categoriaService.selecionarTodos();
   }
 }
